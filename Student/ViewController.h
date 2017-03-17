f337ef4facdf33cf156f4d23038bf27093042d63
bai_huifeng <bai.huifeng@jyall.com>
2017-03-17 09:44:52 +0800

    Initial Commit

---
+++ Student/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Student
+//
+//  Created by 葬花桥 on 2017/3/17.
+//  Copyright © 2017年 葬花桥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Student/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Student
+//
+//  Created by 葬花桥 on 2017/3/17.
+//  Copyright © 2017年 葬花桥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
