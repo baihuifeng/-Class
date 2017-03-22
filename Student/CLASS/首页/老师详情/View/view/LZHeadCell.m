350c1429db2d1433a87ad883874f908d2c972bd2
bai_huifeng <bai.huifeng@jyall.com>
2017-03-22 17:40:33 +0800

    bug

--- Student/CLASS/首页/老师详情/View/view/LZHeadCell.m
+++ Student/CLASS/首页/老师详情/View/view/LZHeadCell.m
@@ -14,7 +14,7 @@
     [super awakeFromNib];
     // Initialization code
 }
-
+        
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
 
